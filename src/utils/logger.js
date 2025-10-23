1aeb12c8df3b2c2a7381224423aafa4946aac921
apollo-xwb <your-email@example.com>
2025-10-23 12:41:28 +0200

    added logger system for better debugging and production error tracking

---
+++ src/utils/logger.js
@@ -0,0 +1,37 @@
+const isDevelopment = __DEV__;
+
+export const logger = {
+  log: (...args) => {
+    if (isDevelopment) {
+      console.log(...args);
+    }
+  },
+  
+  warn: (...args) => {
+    if (isDevelopment) {
+      console.warn(...args);
+    }
+  },
+  
+  error: (...args) => {
+    if (isDevelopment) {
+      console.error(...args);
+    } else {
+    }
+  },
+  
+  info: (...args) => {
+    if (isDevelopment) {
+      console.info(...args);
+    }
+  },
+  
+  debug: (...args) => {
+    if (isDevelopment) {
+      console.debug(...args);
+    }
+  }
+};
+
+export default logger;
+
